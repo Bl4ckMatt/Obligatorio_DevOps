7a2bd292535a4311cbef69f846578e306a416cb6
Bl4ckMatt <56125993+Bl4ckMatt@users.noreply.github.com>
2023-10-07 10:25:19 -0300

    Update ej1-procesamiento_archivos.sh

--- ej1-procesamiento_archivos.sh
+++ ej1-procesamiento_archivos.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Uso: $0 [-3] [-t] archivo"
+  exit 1
+fi
+
+absoluta_archivo=$(find "$(pwd)" -type f -name "$1" -print -quit)
+
+if [ -n "$absoluta_archivo" ]; then
+  # Verificar si el archivo tiene permisos de lectura
+  if [ -r "$absoluta_archivo" ]; then
+    echo "Ruta absoluta del archivo: $absoluta_archivo"
+    echo "El archivo tiene permisos de lectura."
+  else
+    echo "Ruta absoluta del archivo: $absoluta_archivo"
+    echo "El archivo no tiene permisos de lectura."
+  fi
+else
+  echo "El archivo no se encontró en la ruta relativa especificada."
+fi
 
